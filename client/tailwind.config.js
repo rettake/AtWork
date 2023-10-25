a5f9471fd42bd83f976b8ab7c33ca76471fd5e2a
Vladislav Okhotnikov <rettakecsgo@gmail.com>
2023-10-25 14:51:40 +0300

    -colors & fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "sixth-color": "#fdfdfd",
+      "fifth-color": "#f4f4f4",
+      "fourth-color": "#dadada",
+      "third-color": "#9c9c9c",
+      "second-color": "#595959",
+      "first-color": "#161616",
+      "accent": "22a0bc",
+    },
   },
   plugins: [],
-}
-
+};
